a2d7e663a9fc9f72d707f6b3db179e895a6284d0
Harshinielangovan57 <harshinielangovan57@gmail.com>
2026-01-03 16:22:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { AddItem } from './Modules/AddItem';
+import { Todos } from './Modules/Todos';
 
 function App() {
   return (
     <div >
       <AddItem/>
+      <Todos />
 
     </div>
   );
